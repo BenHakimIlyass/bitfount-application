6a55730ced4dce57e24bdc8a06cc90d9599156c2
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2021-07-20 11:05:55 +0100

    :fire: Removing trail due to be unused

--- components/index.ts
+++ components/index.ts
@@ -4,6 +4,5 @@ export { default as SequenceCard } from "./sequenceCard";
 export { default as AutoLayout } from "./autoLayout";
 export { default as Container } from "./container";
 export { default as Footer } from "./footer";
-export { default as Trail } from "./trailAnimation";
 export { default as Button } from "./button";
 export { default as Center } from "./center";
